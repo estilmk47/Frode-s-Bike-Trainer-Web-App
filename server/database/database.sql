70f2a1ead7dfda4b6520c977beee103c1cc6897c
Jon Estil Krågebakk <jonekra@ntnu.no>
2024-11-27 13:36:01 +0100

    Remote push, since the prohect has to be moved out of OneDrive :(

---
+++ server/database/database.sql
@@ -0,0 +1,22 @@
+CRATE TABLE();
+
+-- User:
+--- user_key (PK) 10 varchar
+--- username 60 varchar
+--- first_name 254 varchar
+--- last_name 254 varchar
+--- email 254 varchar
+--- birthday datetime
+--- weight decimal(5,2)
+--- ftp decimal(5,2)
+--- gender INT
+--- difficuty
+
+--- user_bike:
+--- 
+
+-- Bike
+--- bike_key (PK) 20 varchar
+--- mesh 20 varchar
+--- texture 20 varchar
+--- material 20 varchar
